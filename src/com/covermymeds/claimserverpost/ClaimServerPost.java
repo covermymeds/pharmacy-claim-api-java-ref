3203ce29a6bb0b4373a26e2d1f95c01234da0026
jroman <jroman@covermymeds.com>
2012-07-02 16:06:43 -0400

    Code seperated into several functions all having their own task

--- src/com/covermymeds/claimserverpost/ClaimServerPost.java
+++ src/com/covermymeds/claimserverpost/ClaimServerPost.java
@@ -1,5 +1,8 @@
 package com.covermymeds.claimserverpost;
 
+//TODO Move private functions to utility class and check 
+// for null addresses before trying to open them as well
+// as check if suppressed
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -28,19 +31,16 @@ import com.beust.jcommander.ParameterException;
 
 /*
  * Http Post to a claim Server and open point browser to response
+ * 
+ * Apache HttpClient 4.2
+ * 
+ * JCommander 1.26
+ * 
+ * 
  */
 public class ClaimServerPost {
 
 	private static Map<Integer, String> errors = createErrors();
-	private static final int FAIL = 1;
-	private static final int EMPTYRESPONSE = 2;
-
-	// Parameters to send in POST
-	private static String url = null;
-	private static String username = null;
-	private static String password = null;
-	private static String claim = null;
-	private static String apiKey = null;
 
 	/*
 	 * Sample Post request to claims server.
@@ -48,152 +48,68 @@ public class ClaimServerPost {
 	public static void main(String[] args) throws ClientProtocolException,
 			IOException, URISyntaxException {
 
-		// Parse commandLine options
-		JCommandLine parsedObject = null;
-		try {
-			parsedObject = buildParser(args);
-		} catch (ParameterException e) {
-			System.err.println("Error: " + e.getMessage());
-			printUsage(new JCommandLine());
-			System.exit(FAIL);
-		}
+		JCommanderObject parsedObject = ClaimServerPost.parseCommandLine(args);
 
 		// Validate that a claim is present
-		if (claimSupplied(parsedObject)) {
-			// Assign values to POST parameters
-			url = parsedObject.getService_url();
-			username = parsedObject.getUsername();
-			password = parsedObject.getPassword();
-			apiKey = parsedObject.getApiKey();
-			claim = getClaim(parsedObject);
+		if (parsedObject != null && ClaimServerPost.claimSupplied(parsedObject)) {
 
-			// Create an instance of HttpClient.
-			HttpClient client = new DefaultHttpClient();
+			UrlEncodedFormEntity encodedParameters = ClaimServerPost
+					.encodeParameters(parsedObject);
 
-			// Creat and Encode parameters
-			List<BasicNameValuePair> formparams = Arrays.asList(
-					new BasicNameValuePair("username", username),
-					new BasicNameValuePair("password", password),
-					new BasicNameValuePair("ncpdp_claim", claim),
-					new BasicNameValuePair("physician_fax", apiKey));
-			UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams,
-					"UTF-8");
-
-			// Create HttpPost and set its Entity
-			HttpPost httpPost = new HttpPost(url);
-			httpPost.setEntity(entity);
-			boolean failed = false;
-			try {
-				// Execute POST with BasicResponseHandler object
-				String response = client.execute(httpPost,
-						new BasicResponseHandler());
-
-				// Check for empty response;
-				if (response.isEmpty()) {
-					System.out.println("Warning: Empty response, check parameters entered.");
-					System.exit(EMPTYRESPONSE);
-				} else {
-					String[] addresses = response.split("\n");
-
-					// If supported and suppress not present, open browser and
-					// point to each address
-					if (parsedObject.isSuppress()) {
-						if (parsedObject.isVerbose()) {
-							for (String address : addresses) {
-								System.out.println("Request created at: "+ address);
-							}
-						} else {
-							if (Desktop.isDesktopSupported()) {
-								Desktop desktop = Desktop.getDesktop();
-								if (desktop.isSupported(Desktop.Action.BROWSE)) {
-									for (String address : addresses) {
-										if (parsedObject.isVerbose()) {
-											System.out.println("Opening browser to: " + address);
-										}
-										desktop.browse(new URI(address));
-									}
-								} else {
-									System.err.println("Browse action is not supported.");
-									failed = true;
-								}
-							} else {
-								System.err.println("Desktop is not supported.");
-								failed = true;
-							}
-						}
-					}
-				}
-			}
-			// Catch any errors(400,500...) and handle them as appropriate
-			catch (HttpResponseException e) {
-				if (parsedObject.isVerbose()) {
-					System.err.println(e.getStatusCode() + errors.get(e.getStatusCode()));
-				} else {
-					System.err.println(e.getStatusCode());
-				}
-				failed = true;
-			}
-			// Catch generally any malformed urls passed to HttpPost object
-			catch (IllegalStateException e) {
-				System.err.println("Error: " + e.getMessage()
-						+ " Check server url. Value passed was "
-						+ parsedObject.getService_url());
-				failed = true;
-			} finally {
-				// Close all connections and free up system resources
-				client.getConnectionManager().shutdown();
-				if (failed) {
-					System.exit(FAIL);
-				}
+			List<String> addresses = ClaimServerPost.sendHttpRequest(
+					parsedObject.getService_url(), encodedParameters,
+					parsedObject.isVerbose());
+			if(!parsedObject.isSuppress()) {
+				ClaimServerPost.openBrowser(addresses, parsedObject.isVerbose());
 			}
 		} else {
 			System.err
 					.println("Error: must specify a claim file argument or '-' to read from stdin");
-			System.exit(FAIL);
 		}
-	}
-
-	/*
-	 * Parses arguments and returns an object containing the values for each
-	 * option
-	 */
-	private static JCommandLine buildParser(String[] args)
-			throws ParameterException {
-		JCommandLine parsedObject = new JCommandLine();
-		//Parsed Object by calling JCommander Constructor
-		new JCommander(parsedObject, args);
-		return parsedObject;
 	}
 
 	/*
 	 * Checks that a claim is present as either a file or directly as a String
 	 */
-	private static boolean claimSupplied(JCommandLine parsedObject) {
+	private static boolean claimSupplied(JCommanderObject parsedObject) {
 		File claimFile = parsedObject.getClaimInFile();
-		return ((claimFile != null && claimFile.exists()) || parsedObject.readFromStdin());
+		return ((claimFile != null && claimFile.exists()) || parsedObject
+				.readFromStdin());
 	}
 
 	/*
 	 * Returns the claim as a String
 	 */
-	private static String getClaim(JCommandLine parsedObject)
+	private static String getClaim(File claimFile, boolean readFromStdin)
 			throws IOException {
-		if (parsedObject.readFromStdin()) {
+		if (readFromStdin) {
 			System.out.println("Enter claim:");
 			Scanner stdin = new Scanner(System.in);
-			return stdin.nextLine();
-		} else {
-			File claimFile = parsedObject.getClaimInFile();
-			BufferedReader reader = new BufferedReader(
-					new FileReader(claimFile));
-			char[] buf = new char[1024];
-			int numRead = 0;
-			StringBuilder fileData = new StringBuilder();
-			while ((numRead = reader.read(buf)) != -1) {
-				String readData = String.valueOf(buf, 0, numRead);
-				fileData.append(readData);
+			String input = stdin.nextLine();
+			if(input.length() >= 2 && input.charAt(0) != (char)02) {
+				input = (char)02 + input;
 			}
-			return fileData.toString();
+			if(input.length() >= 2 && input.charAt(input.length()-1) != (char)03) {
+				input += (char)03;
+			}
+			return input;
+		} else {
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new FileReader(claimFile));
+				char[] buf = new char[1024];
+				int numRead = 0;
+				StringBuffer fileData = new StringBuffer();
+				while ((numRead = reader.read(buf)) != -1) {
+					String readData = String.valueOf(buf, 0, numRead);
+					fileData.append(readData);
+				}
+				return fileData.toString();
+			} finally {
+				if (reader != null) {
+					reader.close();
+				}
+			}
 		}
 	}
 
@@ -202,25 +118,33 @@ public class ClaimServerPost {
 	 */
 	private static Map<Integer, String> createErrors() {
 		Map<Integer, String> result = new HashMap<Integer, String>();
-		result.put(400, ". Oops, there was a connection problem. Please"
+		result.put(400, "Oops, there was a connection problem. Please"
 				+ " try one more time, then contact CoverMyMeds at 1-866-452-"
 				+ "5017/help@covermymeds.com and they will help you diagnose"
 				+ " this issue.");
-		result.put(403, ".Oops, login failed for the username or password that"
+		result.put(
+				403,
+				"Oops, login failed for the username or password that"
 						+ " was submitted. Please check the username and password in your"
 						+ " account settings in your Pharmacy System and at the CMM website to"
 						+ " make sure they match. If you still have trouble, please contact"
 						+ " CoverMyMeds at 1-866-452-5017/help@covermymeds.com and they will"
 						+ " help you fix this issue.");
-		result.put(404, ". Oops, there was a problem. Please check the username and"
+		result.put(
+				404,
+				"Oops, there was a problem. Please check the username and"
 						+ " password in your account settings in your Pharmacy System and at the"
 						+ " CMM website to make sure they match. If you still have trouble, please"
 						+ " contact CoverMyMeds at 1-866-452-5017/help@covermymeds.com and they will"
 						+ " help you fix this issue.");
-		result.put(408, ". Oops, there was a timeout. Please try the request again in one"
+		result.put(
+				408,
+				"Oops, there was a timeout. Please try the request again in one"
 						+ " minute. If you still have trouble, please contact CoverMyMeds at 1-866-452"
 						+ "-5017/help@covermymeds.com and they will help you fix this issue.");
-		result.put(500, ". Oops, there was a problem. Please try the request again in one minute."
+		result.put(
+				500,
+				"Oops, there was a problem. Please try the request again in one minute."
 						+ " If you still have trouble, please contact CoverMyMeds at 1-866-452-5017"
 						+ "/help@covermymeds.com and they will help you diagnose this issue.");
 		return Collections.unmodifiableMap(result);
@@ -233,5 +157,86 @@ public class ClaimServerPost {
 		JCommander help = new JCommander(object);
 		help.setProgramName("ClaimServerPost");
 		help.usage();
+	}
+
+	private static JCommanderObject parseCommandLine(String[] args) {
+		// Parse commandLine options
+		JCommanderObject parsedObject = null;
+		try {
+			parsedObject = new JCommanderObject();
+			// Parsed Object by calling JCommander Constructor
+			new JCommander(parsedObject, args);
+		} catch (ParameterException e) {
+			System.err.println("Error: " + e.getMessage());
+			printUsage(new JCommanderObject());
+			parsedObject = null;
+		}
+		return parsedObject;
+	}
+
+	private static UrlEncodedFormEntity encodeParameters(
+			JCommanderObject parsedObject) throws IOException {
+
+		// Creat and Encode parameters
+		List<BasicNameValuePair> params = Arrays.asList(
+				new BasicNameValuePair("username", parsedObject.getUsername()),
+				new BasicNameValuePair("password", parsedObject.getPassword()),
+				new BasicNameValuePair("ncpdp_claim", ClaimServerPost
+						.getClaim(parsedObject.getClaimInFile(),parsedObject.readFromStdin())), new BasicNameValuePair(
+						"physician_fax", parsedObject.getApiKey()));
+
+		return new UrlEncodedFormEntity(params, "UTF-8");
+	}
+
+	private static List<String> sendHttpRequest(String serviceUrl,
+			UrlEncodedFormEntity encodedParameters, boolean verbose)
+			throws ClientProtocolException, IOException {
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost(serviceUrl);
+		httpPost.setEntity(encodedParameters);
+
+		List<String> addresses = null;
+		try {
+			// Execute POST with BasicResponseHandler object
+			String response = client.execute(httpPost,
+					new BasicResponseHandler());
+
+			addresses = Arrays.asList(response.split("\n"));
+
+		}
+		// Catch any errors(400,500...) and handle them as appropriate
+		catch (HttpResponseException e) {
+			String errorMessage = String.valueOf(e.getStatusCode()) + " "
+					+ e.getMessage();
+			errorMessage = (verbose ? errorMessage + " "
+					+ errors.get(e.getStatusCode()) : errorMessage);
+			System.err.println(errorMessage);
+		} finally {
+			// Close all connections and free up system resources
+			client.getConnectionManager().shutdown();
+		}
+		return addresses;
+	}
+
+	private static void openBrowser(List<String> addresses, boolean verbose)
+			throws IOException, URISyntaxException {
+		// If supported and suppress not present, open browser and
+		// point to each address
+		if (Desktop.isDesktopSupported()) {
+			Desktop desktop = Desktop.getDesktop();
+			if (desktop.isSupported(Desktop.Action.BROWSE)) {
+				for (String address : addresses) {
+					if (verbose) {
+						System.out.println("Opening browser to: " + address);
+					}
+					desktop.browse(new URI(address));
+				}
+			} else {
+				System.err.println("Browse action is not supported.");
+			}
+		} else {
+			System.err.println("Desktop is not supported.");
+		}
 	}
 }
